54ed53c42bd12c5f45b2529bc2a206c31e986362
Collins <ckorir3@gmail.com>
2026-01-10 23:56:48 +0300

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tutor-mycustompages",
+    version="0.1.0",
+    packages=find_packages(),
+    entry_points={
+        "tutor.plugin.v1": [
+            "mycustompages = tutor_mycustompages",
+        ],
+    },
+)
+
