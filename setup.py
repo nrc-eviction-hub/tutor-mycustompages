30012b19803ec62ae1bf6d159d55ba6e1996d0b1
Collins <ckorir3@gmail.com>
2026-01-11 11:32:22 +0300

    Added hook support info to __init__.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tutor-mycustompages",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     entry_points={
         "tutor.plugin.v1": [
