6476bc6085985d182d8dff443b7ac65c7ebd802d
Collins <ckorir3@gmail.com>
2026-01-11 13:10:06 +0300

    Updated typo in plugins.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="tutor-mycustompages",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
     entry_points={
-        "tutor.plugin.v1": [
+        "tutor.plugins.v1": [
             "mycustompages = tutor_mycustompages",
         ],
     },
